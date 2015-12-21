eaaa8245761671b9ee250cbd8d261dab7e21a96d
Rohan <09rohanchopra@gmail.com>
2015-12-21 21:10:41 +0530

    Started working on cross proc comm

--- gui.py
+++ gui.py
@@ -1,23 +1,62 @@
 #!/usr/bin/python3
 
-
+import os
+import signal
 import sys
 import subprocess
+import time
+import matplotlib
+
+import zmq
+from  multiprocessing import Process
+
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from PyQt5.uic import loadUiType
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import  QThread, pyqtSignal
 from PyQt5 import QtGui, QtCore
 
 
 Ui_MainWindow, QMainWindow = loadUiType('mainwindow.ui')
 
- 
+
+    
+xs=[]
+ys=[] 
+
+
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
 
   def __init__(self):
     super(MainWindow,self).__init__()
     self.setupUi(self)
+    self.fig = plt.figure()
+    self.ax1 = self.fig.add_subplot(1,1,1)
+    self.canvas = FigureCanvas(self.fig)
     
+    
+ 
+    
+    self.zmqThread = zmqWorker()
+    self.zmqThread.mySignal.connect(self.print_message)
+    
+    
+    
+    #self.temperatureLayout.addWidget(self.canvas)
+    
+    
+    self.workerThread = Test()
+    self.workerThread.mySignal.connect(self.on_change)
+    
+    self.portEdit.setText('/dev/ttyACM0')
+    self.baudRateEdit.setText('9600')
     self.failsafeCheck.stateChanged.connect(self.fail_safe)
     self.connectButton.clicked.connect(self.connect_cansat)
     self.telemetryButton.clicked.connect(self.telemetry_toggle)
@@ -29,12 +68,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     self.sensorAltitudeCheck.stateChanged.connect(self.sensor_altitude)
     self.gpsAltitudeCheck.stateChanged.connect(self.gps_altitude)
     
+  def print_message(self,text):
+    print(text)
+  
+  def on_change(self,text):
+    global xs
+    global ys
+    print("Done!"+text+xs[0]+ys[0])
+    self.ax1.clear()
+    self.canvas.draw()
+    self.ax1.plot(xs, ys)
+    self.ax1.plot(ys, xs)
+    self.canvas.draw()
+    
   def connect_cansat(self):
-    #subprocess.Popen("python3 receiveData.py --port /dev/", shell=True)
+    port = self.portEdit.text()
+    baud = self.baudRateEdit.text()
+    self.pro = subprocess.Popen("python3 receiveData.py --port "+port+" --baud "+baud, shell=True)
+    self.zmqThread.start()
+    #subprocess.Popen("python3 iprint.py", shell=True)
     pass
   def telemetry_toggle(self):
+    self.workerThread.start()
     pass
   def deploy_failsafe(self):
+    self.workerThread.terminate()
     pass
   def in_temp(self):
     pass
@@ -63,6 +121,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                               QMessageBox.Yes | QMessageBox.No)
     if choice == QMessageBox.Yes:
       print("Exiting")
+      #work_receiver.close()
+      #context.Term()
+      #self.results_sender.close()
+      #zmq_ctx_destroy()
       sys.exit()
     else:
       pass
@@ -70,10 +132,57 @@ class MainWindow(QMainWindow, Ui_MainWindow):
   def closeEvent(self, event):
     event.ignore()
     self.close_application()
+   
+class Test(QThread):
+  
+  mySignal = pyqtSignal(str) 
+  def __init__(self):
+      super().__init__()
+
+  def run(self):
     
+    graph_data = open('samplefile.txt','r').read()
+    lines = graph_data.split('\n')
+    global xs
+    global ys
+    global ax1
+    for line in lines:
+      if len(line) > 1:
+        x, y = line.split(',')
+        xs.append(x)
+        ys.append(y)
+    
+    self.mySignal.emit("update")
+    
+    
+class zmqWorker(QThread):
+  
+  mySignal = pyqtSignal(str) 
+  def __init__(self):
+      super().__init__()
+      
+      # Initialize a zeromq context
+      context = zmq.Context()
+
+      # Set up a channel to receive work from the ventilator
+      self.work_receiver = context.socket(zmq.REQ)
+      self.work_receiver.connect('tcp://127.0.0.1:5547')
+
+  def run(self):
+    while True:
+      self.work_receiver.send('1'.encode("utf-8"))
+      work_message = self.work_receiver.recv()    
+      self.mySignal.emit(work_message.decode("utf-8"))
+    
+  def __del__(self):
+      print("here")
+      self.work_receiver.close()
+      self.context.term()
+      #context.Term()
 if __name__ == '__main__':
   app = QApplication(sys.argv)
   
   GUI = MainWindow()
+  
   GUI.show()
   sys.exit(app.exec_())
--- receiveData.py
+++ receiveData.py
@@ -2,9 +2,12 @@
 
 import os
 import serial 
+import sys
 import argparse
 import time
 
+import zmq
+from  multiprocessing import Process
 
 
 # create parser
@@ -15,41 +18,72 @@ parser.add_argument('--baud', dest='baud', required=True)
 # parse args
 args = parser.parse_args()
 strPort = args.port
-baud = args.baud
-
-comm = serial.Serial(strPort, baud, timeout=0, xonxoff=False, rtscts=False, dsrdtr=False)
-
-
-
+strbaud = args.baud
+try:
+  comm = serial.Serial(strPort, strbaud, timeout=0, xonxoff=False, rtscts=False, dsrdtr=False)
+except:
+  print("Bad port")
+  sys.exit()
+print ("baud = "+strbaud+"\nport = "+strPort+"\nStarted Receiving")
+line = ""
 class storage:
   def __init__(self):
       
+    # Initialize a zeromq context
+    self.context = zmq.Context()
+ 
+    # Set up a channel to send work
+    self.ventilator_send = self.context.socket(zmq.REP)
+    self.ventilator_send.bind('tcp://127.0.0.1:5547')
+    
     if not os.path.exists("test.csv"):
       with open("test.csv", "a") as myfile:
         myfile.write("<TEAM ID>,<PACKET COUNT>,<ALT SENSOR>,<PRESSURE>,<SPEED>,<TEMP>,<VOLTAGE>,<GPS LATITUDE>,<GPS LONGITUDE>,<GPS ALTITUDE>,<GPS SAT NUM>,<GPS SPEED>,<COMMAND TIME>,<COMMAND COUNT>,[<BONUS>]\n")
         
 
   def main(self):
+  
+  
     while(1):
+      try:
+        global line
+        received=""
+        while 1:
+          receive = comm.readline().decode('utf-8').strip('\x00')
+          if "\n" in receive:
+            received = received + receive.split('\n')[0]
+            break
+          received = received + receive
+          
+        readings = received.split(',')
+        if(received==""): 
+          continue
+        if(line==received):
+          continue
+        if(readings[0]!='132'):
+          continue
+        #TODO acknowledge
+        #comm.write(("Received"+str(readings[1])).encode('utf-8'));
+        with open("test.csv", "a") as myfile:
+          myfile.write(received)
+        line = received
+        #line = received.split('\n')[0];
       
-      
-      received = comm.readline().decode('utf-8').strip('\x00')
-      readings = received.split(',')
-      if(received==""): 
-        continue
-      #TODO acknowledge
-      #comm.write(("Received"+str(readings[1])).encode('utf-8'));
-      with open("test.csv", "a") as myfile:
-        myfile.write(received)
-      line = received.split('\n')[0];
-    
-      print("Line = "+line)
-      
-      #time.sleep(1)
-      #TODO cross thread communication
-
-
-      
+        #print("Line = "+line)
+        
+       
+        #TODO cross thread communication
+        
+       
+        msg = self.ventilator_send.recv()
+        self.ventilator_send.send(line.encode("utf-8"))
+        
+        
+        
+        time.sleep(0.5)
+      except:
+        self.ventilator_send.close()
+        #self.context.Term()
       
   def triggerFailsafe(self):
     #TODO failsafe
@@ -61,6 +95,9 @@ class storage:
     #TODO reconnect
     print("Reconnected")
   
+  def __del__(self):
+    self.ventilator_send.close()
+    self.context.term()
 
 if __name__=="__main__":
   store = storage()
